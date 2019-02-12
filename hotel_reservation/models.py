8e86224ebc9fc4bdc8849da79eb2a076e0be778e
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-12 17:17:42 +0200

    Add class hotel

--- hotel_reservation/models.py
+++ hotel_reservation/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from places import models as City
 # Create your models here.
+class Hotel(models.Model):
+    hotel_name=models.CharField(max_length=200)
+    hotel_ranking=models.IntegerField()
+    city_id=models.ForeignKey(City)
