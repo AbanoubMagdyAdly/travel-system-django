a129452c89f38d13eaf4317521334408ae341db2
Asmaa Saeed <asmaasaeed057@gmail.com>
2019-02-12 18:27:46 +0200

    Add class models(comment & user experiences)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Comments(models.Model):
+	user_experience_id=models.ForeignKey(User_Experiences)
+	user_id=models.ForeignKey(Users)
+	comment=models.CharField(max_length=200)
+
+class User_Experiences(models.Model):
+	city_id=models.ForeignKey(Cities)
+	user_id=models.ForeignKey(Users)
+	description=CharField(max_length=1000)
+
