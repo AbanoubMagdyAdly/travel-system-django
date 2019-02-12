47f9c163dc09c42182bf8c49fe17c9b6bfa8fa7e
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-12 18:49:56 +0200

    refactor classes

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from places import models as Cities
 # Create your models here.
+
+
+class User_Experiences(models.Model):
+	city_id=models.ForeignKey(Cities.Cities)
+	user_id=models.ForeignKey(Users)
+	description=CharField(max_length=1000)
 
 class Comments(models.Model):
 	user_experience_id=models.ForeignKey(User_Experiences)
 	user_id=models.ForeignKey(Users)
 	comment=models.CharField(max_length=200)
-
-class User_Experiences(models.Model):
-	city_id=models.ForeignKey(Cities)
-	user_id=models.ForeignKey(Users)
-	description=CharField(max_length=1000)
-
