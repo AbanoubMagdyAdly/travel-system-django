137652dd008fd139af5a0e422ccda282eabb326a
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-12 17:52:39 +0200

    refactor classes

--- hotel_reservation/models.py
+++ hotel_reservation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from places import models as City
+from places import models as Cities
 # Create your models here.
 class Hotel(models.Model):
     hotel_name=models.CharField(max_length=200)
     hotel_ranking=models.IntegerField()
-    city_id=models.ForeignKey(City)
+    city_id=models.ForeignKey(Cities)
--- places/models.py
+++ places/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Locations(models.Model):
-    location_name=models.CharField(max_length=200)
-    city_id=models.ForeignKey(Cities)
-
 class Countries(models.Model):
 	country_name=models.CharField(max_length=200)
 
 class Cities(models.Model):
 	city_name=models.CharField(max_length=200)
 	country_id=models.ForeignKey(Countries)
-	
-		
-	
-	
+
+class Locations(models.Model):
+    location_name=models.CharField(max_length=200)
+    city_id=models.ForeignKey(Cities)
+
