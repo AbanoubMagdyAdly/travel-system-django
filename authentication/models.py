b7c868dde248d2cfe71f0286fcb4dd1e81065645
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-13 17:20:42 +0200

    change class

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
-    user_name = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    is_admin = models.BooleanField
-    is_blocked = models.BooleanField
