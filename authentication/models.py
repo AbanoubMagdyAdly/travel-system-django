bb79a644547458b16251c722618e3bfff9e6708b
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-14 11:00:44 +0200

    refactor classes

--- authentication/models.py
+++ authentication/models.py
@@ -5,20 +5,20 @@ class Hotels(models.Model):
 
 class Users(models.Model):
     user_name = models.CharField(max_length = 100)
-    user_password = models.CharField(max_length = 64)
-    user_email = models.CharField(max_length = 50)
-    user_is_blocked = models.BooleanField()
+    user_password = models.CharField(max_length = 64 , default=None)
+    user_email = models.CharField(max_length = 50 , default=None)
+    user_is_blocked = models.BooleanField
 
 
 class Reservations(models.Model):
-    user = models.ForeignKey(Users)
-    hotel = models.ForeignKey(Hotels)
+    user = models.ForeignKey(Users,on_delete=None)
+    hotel = models.ForeignKey(Hotels,on_delete=None)
     no_of_persons = models.IntegerField()
     from_date = 0
     to_date = 0
 
 class Rides(models.Model):
-    user = models.ForeignKey(Users)
+    user = models.ForeignKey(Users,on_delete=None)
     ride_date = 0
     ride_time = 0
     from_location = 0
--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,16 @@
 from django.db import models
 from places import models as Cities
-<<<<<<< HEAD
-from authentication.models import Users
-=======
 from authentication import models as Users
 
-
->>>>>>> b7c868dde248d2cfe71f0286fcb4dd1e81065645
 # Create your models here.
 
 
 class User_Experiences(models.Model):
-<<<<<<< HEAD
-	city_id=models.ForeignKey(Cities.Cities)
-	user_id=models.ForeignKey(Users)
-	description=models.CharField(max_length=1000)
-=======
+
     city_id = models.ForeignKey(Cities.Cities, on_delete=None)
     user_id = models.ForeignKey(Users.Users, on_delete=None)
     description = models.CharField(max_length=1000)
 
->>>>>>> b7c868dde248d2cfe71f0286fcb4dd1e81065645
 
 class Comments(models.Model):
     user_experience_id = models.ForeignKey(User_Experiences, on_delete=None)
