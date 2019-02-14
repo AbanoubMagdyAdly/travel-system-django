19fa032c3416eca13a32229ab0e4d0085619b9c7
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-14 11:35:10 +0200

    Modify database

--- car_rental/models.py
+++ car_rental/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication import models as Users
 
 # Create your models here.
+class Rides(models.Model):
+    ride_date = models.DateField(default=None)
+    ride_time = models.TimeField(default=None)
+    user_id = models.ForeignKey(Users.Users, on_delete=None)
+    from_location = models.IntegerField(default=0)
+    to_location = models.IntegerField(default=0)
--- hotel_reservation/models.py
+++ hotel_reservation/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from authentication import models as Users
+from places import models as Hotels
 
 
 # Create your models here.
 
+class Reservations(models.Model):
+    user = models.ForeignKey(Users.Users, on_delete=None)
+    hotel = models.ForeignKey(Hotels.Hotel, on_delete=None)
+    no_of_persons = models.IntegerField()
+    from_date = models.DateField(default=None)
+    to_date = models.DateField(default=None)
